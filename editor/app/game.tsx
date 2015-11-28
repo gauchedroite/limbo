0c5f7fe1ab214a8904944922b35c08e03e72dad9
gauchedroite <ctrepanier@gmail.com>
2015-11-28 16:45:48 -0500

    content editable

--- editor/app/game.tsx
+++ editor/app/game.tsx
@@ -1,13 +1,13 @@
 
 import React = require("react");
+import ContentEditable from "../components/react-contenteditable";
 import State = require( "./state");
 import "./reactions";
-
 
 export class Actor extends React.Component<any, any> {
     render() {
         return (
-            <div>A C T O R</div>
+            <h1>A C T O R</h1>
         );
     }
 }
@@ -15,26 +15,41 @@ export class Actor extends React.Component<any, any> {
 export class Location extends React.Component<any, any> {
     render() {
         return (
-            <div>L O C A T I O N</div>
+            <h1>L O C A T I O N</h1>
         );
     }
 }
 
 export class Scene extends React.Component<any, any> {
     fu = () => { this.forceUpdate(); };
+    
+    constructor() {
+        super();
+        this.state = { html: "chaval" };
+    }
+    
     componentDidMount() {
         State.on("update", this.fu);
-        //State.trigger("scene:fetch", 0);
+        State.trigger("scene:fetch", 0);
     }
+    
     componentWillUnmount() {
         State.off("update", this.fu);
     }
+    
+    handleChange(evt: any) {
+        this.setState({ html: evt.target.value });
+    }
+    
     render() {
         var state = State.get();
         return (
             <div>
-                <span>S C E N E</span>
-                <div>De bonne humeur et {state.status}</div>
+                <h1>S C E N E is {state.status}</h1>
+                <ContentEditable 
+                    html = {this.state.html} 
+                    disabled = {false} 
+                    onChange = {this.handleChange.bind(this)} />
             </div>
         );
     }
